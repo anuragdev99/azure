c1036588536891e1756131d80423f2bb48767181
anuragdev99 <anuragdevops99@gmail.com>
2025-07-13 19:38:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,6 +45,13 @@ resource "azurerm_linux_virtual_machine" "ubuntu" {
     public_key = var.ssh_public_key
   }
 
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
+  }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
