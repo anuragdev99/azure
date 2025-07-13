6158d2e790eb525075e724a5679f61dd75ed63fa
anuragdev99 <anuragdevops99@gmail.com>
2025-07-13 19:21:11 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstate123tuffs"
+    container_name       = "tfstate"
+    key                  = "ubuntu-vm-keyvault.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
