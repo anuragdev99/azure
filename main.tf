ec67fd24c0f0c991ff6584f5f8ed230bdfa68761
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 12:09:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
-module "network" {
-  source = "./modules/network"
-}
+ module "network" {
+   source = "./modules/network"
+ }
 
-module "vm" {
-  source         = "./modules/vm"
-  ssh_public_key = var.ssh_public_key
-}
+ module "vm" {
+   source         = "./modules/vm"
+   ssh_public_key = var.ssh_public_key
 
-module "keyvault" {
-  source       = "./modules/keyvault"
-  tenant_id    = var.tenant_id
-  vm_object_id = module.vm.vm_object_id
-}
+   rg_name        = module.network.rg_name
+   location       = module.network.location
+ }
+
+ module "keyvault" {
+   source       = "./modules/keyvault"
+   tenant_id    = var.tenant_id
+   vm_object_id = module.vm.vm_object_id
+
+   rg_name      = module.network.rg_name
+   location     = module.network.location
+ }
 
