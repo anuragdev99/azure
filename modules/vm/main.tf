536034aa9ed333fbbe4f8d8dfd1ed0af2debdbf7
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 13:02:21 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
