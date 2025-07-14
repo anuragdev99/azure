e974af0216b71aa88308a0459495f597a68a0b7b
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 10:23:46 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_public_ip" {
-  description = "The public IP address assigned to the Ubuntu VM"
-  value       = azurerm_public_ip.vm.ip_address
-}
-
-output "keyvault_uri" {
-  description = "The URI of the Azure Key Vault"
-  value       = azurerm_key_vault.vault.vault_uri
-}
