72ee3e98a49e3b6804118cfd3717ddaba5c252f6
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 11:32:08 +0530

    Create network.tf

---
+++ resources/network.tf
@@ -0,0 +1,43 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vm-keyvault"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-main"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-main"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-main"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  dynamic "security_rule" {
+    for_each = [
+      { name = "SSH"   , port = 22  , priority = 1001 },
+      { name = "HTTP"  , port = 80  , priority = 1002 },
+      { name = "HTTPS" , port = 443 , priority = 1003 }
+    ]
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = tostring(security_rule.value.port)
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  }
+}
