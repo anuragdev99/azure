19af7e972a93477eae62fd5b467fb904cdf78a81
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 13:00:05 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -7,3 +7,8 @@ output "location" {
   description = "Resource group location"
   value       = azurerm_resource_group.rg.location
 }
+
+output "subnet_id" {
+  description = "Subnet ID for VM NIC"
+  value       = azurerm_subnet.subnet.id
+}
