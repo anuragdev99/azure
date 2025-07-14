bf726096d355f2e42d430aff9a9fdd3513909480
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 13:49:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "tenant_id" {
   description = "Azure Tenant ID (for Key Vault)"
   type        = string
 }
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
