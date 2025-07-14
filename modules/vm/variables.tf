c7747b22167e640b569be167a57b67c20bfc9dde
anuragdev99 <anuragdevops99@gmail.com>
2025-07-14 12:10:07 +0530

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -2,3 +2,13 @@ variable "ssh_public_key" {
   description = "SSH public key for VM login"
   type        = string
 }
+
+variable "rg_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Resource group location"
+  type        = string
+}
