936d97770051d027ea99fd856fb12dca3574b465
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 17:29:02 +0530

    Update providers.tf

--- cert-download/providers.tf
+++ cert-download/providers.tf
@@ -1,11 +1,7 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0"
-    }
+    azurerm = { source = "hashicorp/azurerm" version = ">= 3.0" }
   }
-
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "tfstate123tuffs"
@@ -16,8 +12,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_oidc        = true
+
+  use_oidc        = true            # <<< Force OIDC authentication
   subscription_id = var.subscription_id
-  client_id       = var.client_id
   tenant_id       = var.tenant_id
+  client_id       = var.client_id
 }
