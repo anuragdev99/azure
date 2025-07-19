f10c1cfcf4e10fa14b8ee44db67764d803d60250
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 17:29:27 +0530

    Update variables.tf

--- cert-download/variables.tf
+++ cert-download/variables.tf
@@ -1,4 +1,19 @@
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
-variable "vm_id" {}
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Federated OIDC App (client) ID"
+  type        = string
+}
+
+variable "vm_id" {
+  description = "Full resource ID of the target VM to extend"
+  type        = string
+}
