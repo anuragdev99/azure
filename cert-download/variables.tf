2b3c8e2a31dca5e3413533141c8864b36feb8ba0
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 18:11:28 +0530

    Update variables.tf

--- cert-download/variables.tf
+++ cert-download/variables.tf
@@ -16,4 +16,5 @@ variable "client_id" {
 variable "vm_id" {
   description = "Resource ID of the VM"
   type        = string
+  default     = "/subscriptions/304f61b6-4359-4c2b-ac0b-5c1383e8fb79/resourceGroups/rg-vm-keyvault/providers/Microsoft.Compute/virtualMachines/ubuntu-vm"
 }
