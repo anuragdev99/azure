f80d734e1b317cf2a457a5a2713f697e400d60f4
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 15:58:32 +0530

    Create providers.tf

---
+++ cert-download/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatestorageprod"
+    container_name       = "tfstate"
+    key                  = "cert-download.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
