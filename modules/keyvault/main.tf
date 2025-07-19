530ce1caacefdc66f0872042131560b6be3561a3
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 14:33:47 +0530

    Update main.tf

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,9 +1,3 @@
-resource "random_string" "suffix" {
-  length  = 6
-  upper   = false
-  special = false
-}
-
 resource "azurerm_key_vault" "kv" {
   name                        = "kv-vm-${random_string.suffix.result}"
   location                    = var.location
