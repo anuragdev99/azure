273873b99ead45d456e7f4f6b0093e4e1d692991
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 15:57:42 +0530

    Create main.tf

---
+++ cert-download/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_virtual_machine_extension" "install_cert" {
+  name                 = "install-cert"
+  virtual_machine_id   = var.vm_id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+
+  settings = jsonencode({
+    commandToExecute = "bash -c 'curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash && az login --identity && az keyvault secret download --vault-name kv-vm-z4ahhe --name my-cert --file /tmp/my-cert.pfx --encoding base64'"
+  })
+}
