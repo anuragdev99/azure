4add9620de4f8fe5bb11409c310e15a0ca5e661a
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 17:01:51 +0530

    Update providers.tf

--- cert-download/providers.tf
+++ cert-download/providers.tf
@@ -5,6 +5,7 @@ terraform {
       version = "~> 3.0"
     }
   }
+
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "tfstate123tuffs"
@@ -15,6 +16,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
