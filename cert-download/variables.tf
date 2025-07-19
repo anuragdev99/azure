a2aba71de741a54d147a147e2bf9000662fb816a
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 16:31:48 +0530

    Update variables.tf

--- cert-download/variables.tf
+++ cert-download/variables.tf
@@ -1,12 +1,4 @@
-# Empty or used if parameterized later
-variable "client_id" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
-variable "subscription_id" {
-  type = string
-}
+variable "vm_id" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
