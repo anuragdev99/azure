67e45c97944214c96c47b9eb3afcc860870712b4
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 16:21:17 +0530

    Update providers.tf

--- cert-download/providers.tf
+++ cert-download/providers.tf
@@ -16,4 +16,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  use_oidc        = true
 }
