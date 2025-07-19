022d1fff9b05a8b6e0986664ab33166565b46399
anuragdev99 <anuragdevops99@gmail.com>
2025-07-19 14:33:21 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -58,7 +58,8 @@ resource "azurerm_virtual_machine_extension" "install_cert" {
   type_handler_version = "2.1"
 
 settings = jsonencode({
-  commandToExecute = "bash -c 'curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash && az login --identity && az keyvault secret download --vault-name kv-vm-z4ahhe --name my-cert --file /tmp/my-cert.pfx --encoding base64'"
+  commandToExecute = "bash -c 'curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash && az login --identity --allow-no-subscriptions && az keyvault secret download --vault-name kv-vm-z4ahhe --name my-cert --file /tmp/my-cert.pfx --encoding base64'"
+
 })
 
 }
