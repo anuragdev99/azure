39b9cc0e85341d8529f063eea848e8ace0b87b44
anuragdev99 <anuragdevops99@gmail.com>
2025-07-20 11:06:31 +0530

    Update main.tf

--- cert-download/main.tf
+++ cert-download/main.tf
@@ -1,21 +1,22 @@
-resource "azurerm_virtual_machine_extension" "download_cert" {
-  name                 = "cert-downloader"
-  virtual_machine_id   = var.vm_id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.1"
+data "azurerm_key_vault_secret" "cert" {
+  name         = "my-cert"
+  key_vault_id = var.keyvault_id
+}
 
-  settings = jsonencode({
-    commandToExecute = <<-EOT
-      bash -c '
-        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash &&
-        az login --identity &&
-        az keyvault secret download \
-          --vault-name kv-vm-z4ahhe \
-          --name my-cert \
-          --file /tmp/my-cert.pfx \
-          --encoding base64
-      '
+resource "azurerm_virtual_machine_extension" "download_cert" {
+  name                       = "cert-downloader"
+  virtual_machine_id         = var.vm_id
+  publisher                  = "Microsoft.Azure.Extensions"
+  type                       = "CustomScript"
+  type_handler_version       = "2.1"
+
+  protected_settings = {
+    script = <<-EOT
+      #!/bin/bash
+      set -e
+      # data.azurerm_key_vault_secret.cert.value is Base64-encoded by Key Vault
+      echo "${data.azurerm_key_vault_secret.cert.value}" | base64 --decode > /tmp/my-cert.pfx
+      chmod 600 /tmp/my-cert.pfx
     EOT
-  })
+  }
 }
