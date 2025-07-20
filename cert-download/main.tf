084f3a3eb09f033ee981fc60110bda298bbb9c07
anuragdev99 <anuragdevops99@gmail.com>
2025-07-20 11:22:16 +0530

    Update main.tf

--- cert-download/main.tf
+++ cert-download/main.tf
@@ -1,3 +1,11 @@
+// cert-download/main.tf
+
+// 1. Read the secret from Key Vault
+data "azurerm_key_vault_secret" "cert" {
+  name         = "my-cert"
+  key_vault_id = var.keyvault_id
+}
+
 resource "azurerm_virtual_machine_extension" "download_cert" {
   name                 = "cert-downloader"
   virtual_machine_id   = var.vm_id
@@ -5,16 +13,17 @@ resource "azurerm_virtual_machine_extension" "download_cert" {
   type                 = "CustomScript"
   type_handler_version = "2.1"
 
+  // Wrap the script in base64encode() and jsonencode()
   protected_settings = jsonencode({
     script = base64encode(<<-EOT
-    #!/bin/bash
-    set -e
+      #!/bin/bash
+      set -e
 
-    # data.azurerm_key_vault_secret.cert.value is Base64 from Key Vault
-    echo "${data.azurerm_key_vault_secret.cert.value}" \
-      | base64 --decode > /tmp/my-cert.pfx
+      # Terraform has fetched the Base64-encoded secret value
+      echo "${data.azurerm_key_vault_secret.cert.value}" \
+        | base64 --decode > /tmp/my-cert.pfx
 
-    chmod 600 /tmp/my-cert.pfx
+      chmod 600 /tmp/my-cert.pfx
     EOT
     )
   })
