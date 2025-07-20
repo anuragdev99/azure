9ae82a3059ad069649b4f2372b5cb3fb504f8ab1
anuragdev99 <anuragdevops99@gmail.com>
2025-07-20 11:19:17 +0530

    Update main.tf

--- cert-download/main.tf
+++ cert-download/main.tf
@@ -1,10 +1,3 @@
-# cert-download/main.tf
-
-data "azurerm_key_vault_secret" "cert" {
-  name         = "my-cert"
-  key_vault_id = var.keyvault_id
-}
-
 resource "azurerm_virtual_machine_extension" "download_cert" {
   name                 = "cert-downloader"
   virtual_machine_id   = var.vm_id
@@ -12,13 +5,17 @@ resource "azurerm_virtual_machine_extension" "download_cert" {
   type                 = "CustomScript"
   type_handler_version = "2.1"
 
-  # Terraform expects a single string here, so jsonencode() it
   protected_settings = jsonencode({
-    script = <<-EOT
-      #!/bin/bash
-      set -e
-      echo "${data.azurerm_key_vault_secret.cert.value}" | base64 --decode > /tmp/my-cert.pfx
-      chmod 600 /tmp/my-cert.pfx
+    script = base64encode(<<-EOT
+    #!/bin/bash
+    set -e
+
+    # data.azurerm_key_vault_secret.cert.value is Base64 from Key Vault
+    echo "${data.azurerm_key_vault_secret.cert.value}" \
+      | base64 --decode > /tmp/my-cert.pfx
+
+    chmod 600 /tmp/my-cert.pfx
     EOT
+    )
   })
 }
